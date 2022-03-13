6ea1ed1fde9c62106ae0f779a4ffcac5fae74a32
Keks <keks@htmlacademy.ru>
2022-03-13 16:55:21 +0300

    :heavy_check_mark: Сборка #5

---
+++ 5/js/picture.js
@@ -0,0 +1,18 @@
+import {photoMocks} from './data.js';
+
+const userPictures = document.querySelector('.pictures');
+const userPictureTemplate = document.querySelector('#picture')
+  .content
+  .querySelector('.picture');
+
+const userPictureFragment = document.createDocumentFragment();
+
+photoMocks.forEach(({url, likes, comments}) => {
+  const userPictureElement = userPictureTemplate.cloneNode(true);
+  userPictureElement.querySelector('.picture__img').src = url;
+  userPictureElement.querySelector('.picture__likes').textContent = likes;
+  userPictureElement.querySelector('.picture__comments').textContent = comments.length;
+  userPictureFragment.appendChild(userPictureElement);
+});
+
+userPictures.appendChild(userPictureFragment);
