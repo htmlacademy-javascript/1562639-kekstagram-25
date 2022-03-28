7e5157409c16d2368364084c1a886d83d2d4711f
JuliannaLazareva <86108150+JuliannaLazareva@users.noreply.github.com>
2022-03-28 15:27:12 +0300

    Часть задания 8-1

---
+++ js/form.js
@@ -0,0 +1,82 @@
+import { isEscapeKey } from './util.js';
+
+const form = document.querySelector('.img-upload__form');
+const uploadFileInput = form.querySelector('#upload-file');
+const uploadOverlay = form.querySelector('.img-upload__overlay');
+const uploadCancelButton = form.querySelector('#upload-cancel');
+const textHashtags = form.querySelector('.text__hashtags');
+const description = form.querySelector('.text__description');
+
+const pristine = new Pristine(form, {
+  classTo: 'img-upload__text',
+  errorTextParent: 'img-upload__text',
+});
+
+const onRedactorEscKeydown = (evt) => {
+  if (isEscapeKey(evt)) {
+    evt.preventDefault();
+    closeRedactorPhoto();
+  }
+};
+
+function openRedactorPhoto () {
+  uploadOverlay.classList.remove('hidden');
+  document.querySelector('body').classList.add('modal-open');
+}
+
+function closeRedactorPhoto () {
+  uploadOverlay.classList.add('hidden');
+  document.querySelector('body').classList.remove('modal-open');
+
+  uploadFileInput.value = '';
+
+  document.removeEventListener('keydown', onRedactorEscKeydown);
+}
+
+uploadFileInput.addEventListener('change', () => {
+  openRedactorPhoto();
+
+  document.addEventListener('keydown', onRedactorEscKeydown);
+});
+
+uploadCancelButton.addEventListener('click', () => {
+  closeRedactorPhoto();
+});
+
+textHashtags.addEventListener('change', () => {
+  const hashtags = textHashtags.value;
+  const hashtagsArray = hashtags.split(' ', 5);
+  for (let i = 0; i <= hashtagsArray.length - 1; i++) {
+    for (let j = i+1; j <= hashtagsArray.length; j++) {
+      // eslint-disable-next-line eqeqeq
+      if (i != j && hashtagsArray[i] != hashtagsArray[j]) {
+        // eslint-disable-next-line no-console
+        console.log('OK');
+      } else {
+        // eslint-disable-next-line no-console
+        console.log('Error');
+      }
+    }
+  }
+});
+
+
+form.addEventListener('submit', (evt) => {
+  evt.preventDefault();
+
+  const isValid = pristine.validate();
+  if (isValid) {
+    // eslint-disable-next-line no-console
+    console.log('Можно отправлять');
+  } else {
+    // eslint-disable-next-line no-console
+    console.log('Форма невалидна');
+  }
+});
+
+
+// разобраться, куда это вставить
+//if (textHashtags.focus || description.focus) {
+//evt.stopPropagation();
+//}
+
--- js/full-picture.js
+++ js/full-picture.js
@@ -1,19 +1,37 @@
 import { photoMocks } from './data.js';
 import './picture.js';
-import './close-picture.js';
+import { isEscapeKey } from './util.js';
 
 const thumbnails = document.querySelectorAll('.picture');
 const bigPicture = document.querySelector('.big-picture');
 const commentItem = bigPicture.querySelector('.social__comment');
 
-//const closeModalButton = document.querySelector('.big-picture__cancel');
+const closeModalButton = document.querySelector('.big-picture__cancel');
 
-function addThumbnailClickHandler(thumbnail, photoData) {
+const onPopupEscKeydown = (evt) => {
+  if (isEscapeKey(evt)) {
+    evt.preventDefault();
+    closeUserModal();
+  }
+};
+
+function openUserModal () {
+  document.querySelector('body').classList.add('modal-open');
+  bigPicture.classList.remove('hidden');
+}
+
+function closeUserModal () {
+  bigPicture.classList.add('hidden');
+  document.querySelector('body').classList.remove('modal-open');
+
+  document.removeEventListener('keydown', onPopupEscKeydown);
+}
+
+function onThumbnailClick(thumbnail, photoData) {
   thumbnail.addEventListener('click', () => {
+    openUserModal ();
     const commentList = bigPicture.querySelector('.social__comments');
     commentList.textContent = '';
-    document.querySelector('body').classList.add('modal-open');
-    bigPicture.classList.remove('hidden');
     bigPicture.querySelector('.social__comment-count').classList.add('hidden');
     bigPicture.querySelector('.comments-loader').classList.add('hidden');
     bigPicture.querySelector('.big-picture__img').querySelector('img').src = photoData.url;
@@ -27,11 +45,15 @@ function addThumbnailClickHandler(thumbnail, photoData) {
       commentElement.querySelector('.social__text').textContent = message;
       commentList.appendChild(commentElement);
     });
+
+    document.addEventListener('keydown', onPopupEscKeydown);
   });
 }
 
 for (let i = 0; i < thumbnails.length; i++) {
-  addThumbnailClickHandler(thumbnails[i], photoMocks[i]);
+  onThumbnailClick(thumbnails[i], photoMocks[i]);
 }
 
-export {bigPicture};
+closeModalButton.addEventListener('click', () => {
+  closeUserModal();
+});
